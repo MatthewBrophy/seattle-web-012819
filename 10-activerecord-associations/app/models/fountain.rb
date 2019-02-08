4bd5380f0307a8393b56d23c848afa9e33cd1519
sbal13 <sbal13@gmail.com>
2019-02-08 10:49:21 -0800

    mid lecture

--- 10-activerecord-associations/app/models/fountain.rb
+++ 10-activerecord-associations/app/models/fountain.rb
@@ -1,23 +1,2 @@
 class Fountain < ActiveRecord::Base
-
-	attr_accessor :name, :location
-
-	@@all = []
-
-	def initialize(name, location)
-		@name = name
-		@location = location
-
-		@@all << self
-	end
-
-	def self.all
-		@@all
-	end
-
-	def wishes
-		Wish.all.select do |wish|
-			wish.fountain == self
-		end
-	end
 end
--- 10-activerecord-associations/app/models/wish.rb
+++ 10-activerecord-associations/app/models/wish.rb
@@ -1,17 +1,2 @@
 class Wish < ActiveRecord::Base
-
-	attr_accessor :fountain, :wish_maker, :content
-
-	@@all = []
-
-	def initialize(fountain, wish_maker, content)
-		@fountain = fountain
-		@wish_maker = wish_maker
-		@content = content
-		@@all << self
-	end
-
-	def self.all
-		@@all
-	end
 end
